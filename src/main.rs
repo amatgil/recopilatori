2d02a63f0829795effe8e7c1ddba2ea5cbd1fd01
amatgil <amatgilvinyes@gmail.com>
2025-02-10 10:20:50 +0100

    Start with clap

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,22 @@ use std::{path::Path, time::Instant};
 
 use sqlx::sqlite::*;
 
-async fn populate() -> Result<(), sqlx::Error> {
+use clap::*;
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Populate { path_directori_a_tractar: String },
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+async fn populate(folder: &str) -> Result<(), sqlx::Error> {
     // Create a connection pool
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
@@ -12,7 +27,6 @@ async fn populate() -> Result<(), sqlx::Error> {
 
     setup(&pool).await?;
 
-    let folder = "./fitxers_a_tractar";
     for file in recurse_files(Path::new(folder))? {
         let real_path = file.path();
         let db_path = file.path().to_owned();
@@ -35,5 +49,15 @@ async fn populate() -> Result<(), sqlx::Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    populate().await
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Populate {
+            path_directori_a_tractar,
+        }) => populate(&path_directori_a_tractar).await?,
+        None => {
+            println!("T'has deixat la subcomanda (--help per veure-les)");
+            std::process::exit(1);
+        }
+    }
+    Ok(())
 }
