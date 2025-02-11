81a546f5c4b1476fff9704d9592d06998d48846d
amatgil <amatgilvinyes@gmail.com>
2025-02-11 22:41:17 +0100

    Forgot to commit the actual file lmao

---
+++ src/geoloc.rs
@@ -0,0 +1,54 @@
+use sqlx::SqlitePool;
+
+use crate::*;
+use nom_exif::{ExifIter, LatLng, MediaParser, MediaSource, URational};
+
+/// Returns (lat, long)
+fn get_latlong(f: &Path) -> Result<Option<(f64, f64)>, nom_exif::Error> {
+    let mut parser = MediaParser::new();
+    let data = fs::File::open(f)?;
+    let ms = MediaSource::file(data)?;
+
+    if ms.has_exif() {
+        let exif: ExifIter = parser.parse(ms)?;
+        match exif.parse_gps_info() {
+            Ok(Some(info)) => {
+                let r_to_f = |r: URational| r.0 as f64 / r.1 as f64;
+                let dms_to_f = |l: LatLng| r_to_f(l.0) * 3600.0 + r_to_f(l.1) * 60.0 + r_to_f(l.2);
+                let lat = dms_to_f(info.latitude);
+                let lon = dms_to_f(info.longitude);
+                Ok(Some((lat, lon)))
+            }
+            Ok(None) => Ok(None),
+            Err(e) => Err(e),
+        }
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn update_geoloc(pool: &SqlitePool, dir: &Path) -> Result<(), sqlx::Error> {
+    let inner_paths = sqlx::query!("SELECT fitxer_id, full_path FROM fitxers;")
+        .fetch_all(pool)
+        .await?;
+
+    for inner_rec in inner_paths {
+        let inner_path = inner_rec.full_path;
+        let real_path = dir.join(&Path::new(&inner_path));
+        match get_latlong(&real_path) {
+            Ok(Some((lat, long))) => {
+                sqlx::query!(
+                    "INSERT OR REPLACE INTO coords (fitxer_id, latitude, longitude) VALUES (?, ?, ?);",
+                    inner_rec.fitxer_id,
+                    lat,
+                    long
+                )
+                .execute(pool)
+                .await?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
